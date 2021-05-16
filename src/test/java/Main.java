d8524436486c0ce367ad9a1735c75dae5873dd03
Alexandru Chis <alexrareschis@gmail.com>
2021-05-16 19:02:40 +0300

    Initial commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://testfasttrackit.info/selenium-test/customer/account/login/");
+        
+
+
+
+
+
+    }
+}
